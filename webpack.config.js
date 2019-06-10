17c89e1f7d2b60b3311737746ef4fee2c6e62926
Chelsea <galvc@uw.edu>
2019-06-10 11:00:06 -0700

    reinstall npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,40 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        path: __dirname + "/dist",
+        path: __dirname + "/build",
         filename: "bundle.js"
+    },
+    resolve: {
+        extensions: ["", ".js", ".jsx"]
     },
     module: {
         rules: [
             {
                 test: /\.scss$/,
                 use: ["sass-loader", "css-loader", "style-loader"]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
             }
         ]
-    }
+    },
+    devServer: {
+        contentBase: "./build"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve("./index.html")
+        })
+    ]
 };
