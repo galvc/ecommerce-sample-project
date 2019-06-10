2263777ab9536a1eb2cd858e00e63ee9a84f3951
Chelsea <galvc@uw.edu>
2019-06-10 11:06:17 -0700

    Revert "reinstall npm"
    This reverts commit 17c89e1f7d2b60b3311737746ef4fee2c6e62926.

--- src/sample-inventory.js
+++ src/sample-inventory.js
@@ -2,7 +2,6 @@ const inventory = {
     plant1: {
         name: "Money Plant",
         image: "/images/money-tree.jpg",
-        thumb: "/images/thumbnails/thumbnail_money-tree_200px.jpg",
         price: 25.0,
         description:
             "E. aureum is an evergreen vine growing to 20 m (66 ft) tall, with stems up to 4 cm (2 in) in diameter, climbing by means of aerial roots which adhere to surfaces.",
@@ -11,8 +10,6 @@ const inventory = {
     plant2: {
         name: "Snake Plant",
         image: "/images/snake-plant.jpg",
-        thumb: "/images/thumbnails/thumbnail_snake-plant_200px.jpg",
-
         price: 15.0,
         description:
             "A species of flowering plant in the family Asparagaceae, native to tropical West Africa from Nigeria east to the Congo",
@@ -21,8 +18,6 @@ const inventory = {
     plant3: {
         name: "ZZ Plant",
         image: "/images/zz-plant.jpg",
-        thumb: "/images/thumbnails/thumbnail_zz-plant_200px.jpg",
-
         price: 16.0,
         description:
             "The ZZ plant is an aroid that evolved in much drier biomes than its cousin plants",
@@ -31,8 +26,6 @@ const inventory = {
     plant4: {
         name: "Calathea",
         image: "/images/calathea.jpg",
-        thumb: "/images/thumbnails/thumbnail_calathea_200px.jpg",
-
         price: 20.0,
         description:
             "Calathea and their red-green leaves come in medallion and rattlesnake varieties",
@@ -41,12 +34,10 @@ const inventory = {
     plant5: {
         name: "Pothos Jade",
         image: "/images/pothos-jade.jpg",
-        thumb: "/images/thumbnails/thumbnail_pothos-jade_200px.jpg",
-
         price: 24.0,
         description:
             " It requires intermittent watering, fair to middling sun and grows fast",
-        status: "available"
+        status: "sold out"
     }
 };
 
